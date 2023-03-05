e540c2e5d46907cd8bb2d4317acf6d244cb8241f
SANCS99 <nipunchasakalasuriya@gmail.com>
2023-03-05 20:26:54 +0530

    All done
    All done

---
+++ OSGI_ServiceSubscriber/src/osgi_servicesubscriber/ServiceActivator.java
@@ -0,0 +1,29 @@
+package osgi_servicesubscriber;
+
+import com.mtit.service.ServicePublish;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+
+public class ServiceActivator implements BundleActivator {
+
+
+	ServiceReference serviceReference;
+	
+
+	public void start(BundleContext context) throws Exception {
+
+		System.out.println("Start Subscriber Service");
+		serviceReference = context.getServiceReference(ServicePublish.class.getName());
+		ServicePublish servicePublish = (ServicePublish)context.getService(serviceReference);
+		System.out.println(servicePublish.publishService());
+	}
+
+	public void stop(BundleContext context) throws Exception {
+		
+		System.out.println("Good Bye");
+		context.ungetService(serviceReference);
+		
+	}
+
+}
